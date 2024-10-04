1604a2e01125da0c0ec3ae45afeb7557d1bc3c99
xingkun.zhang <1475795322@qq.com>
2024-10-04 16:24:27 +0800

    feat: first version working finue for using local cluster

---
+++ README.md
@@ -0,0 +1,9 @@
+# About
+
+working no when use this version
+
+if docker have something wrong in apple silicon Macbook, need to update docker desktop version and use `softwareupdate --install-rosetta` 
+to update the rosetta 2
+
+Spark panel can not get work detail if not in docker network, so need to expose port to host
+
--- code/hello.py
+++ code/hello.py
@@ -4,13 +4,14 @@ import os
 if __name__ == '__main__':
     print(os.path.dirname(__file__))
     conf = (SparkConf().setAppName('wordCount')
-            .setMaster("spark://localhost:7077"))  # 使用 Docker 主机 IP
+            .setMaster("spark://localhost:7077")
+            )  # 使用 Docker 主机 IP
             # .setMaster("local"))  # 本地运行
 
     sc = SparkContext(conf=conf)
 
     # 确保文件路径正确 --important!!!
-    data = sc.textFile("hdfs://172.18.0.4:8020/wd.txt")
+    data = sc.textFile("hdfs://172.18.0.5:8020/wd.txt")
 
     output = data \
         .flatMap(lambda line: line.split(' ')) \
